6ccfd67538b01030f49947e34d7d2f6773a9dcae
wuiz <wuiz@wuiz-pc.inveadf.gob.mx>
2025-07-07 13:19:54 -0600

    TURNADO

--- Backend-ts/src/routes/deva/personal.routes.ts
+++ Backend-ts/src/routes/deva/personal.routes.ts
@@ -28,16 +28,36 @@ router.get('/personal-turnado', async (req, res) => {
   }
 });
 
-// GET Todos los registros
-router.get('/lcp-turnado', async (req, res) => {
+// GET Registro por ID
+router.get('/lcp-turnado/:id', async (req, res) => {
   try {
+    const id = req.params.id;
     const [rows] = await devaPool.query(`
-      SELECT Pk_IDLCPTurnado, CONCAT(Nombre, '-', Iniciales) AS Nombre
+      SELECT Pk_IDLCPTurnado, CONCAT(Iniciales, '-', Nombre) AS Nombre
       FROM Lcp_Turnado
-    `);
+      WHERE FK_SubAdscrito = ?
+    `, [id]); // Se pasa el parámetro para evitar SQL Injection
+
     res.json({ data: rows });
   } catch (error) {
     console.error(error); // Para depuración
+    res.status(500).json({ error: 'Error en la base de datos' });
+  }
+});
+
+// POST para insertar turno de correspondencia
+router.post('/lcp-turnar', async (req, res) => {
+  try {
+    const { Fk_IDCorrespondenciaIn, Fk_LCP_Turnado } = req.body;
+
+    const [rows] = await devaPool.query(`
+      INSERT INTO Correspondencia_Turnada (Fk_IDCorrespondenciaIn, Fk_LCP_Turnado)
+      VALUES (?, ?);
+    `, [Fk_IDCorrespondenciaIn, Fk_LCP_Turnado]);
+
+    res.json({ success: true, message: 'Turno registrado exitosamente' });
+  } catch (error) {
+    console.error('Error en el INSERT:', error);
     res.status(500).json({ error: 'Error en la base de datos' });
   }
 });
