d62ddedb1583b24b4e638ba8139f707d511d3f04
wuiz <wuiz@wuiz-pc.inveadf.gob.mx>
2025-07-15 15:36:21 -0600

    LISTAS PARA CAPTURISTAS

--- Backend-ts/src/routes/deva/personal.routes.ts
+++ Backend-ts/src/routes/deva/personal.routes.ts
@@ -19,7 +19,7 @@ router.get('/personal-turnado', async (req, res) => {
     const [rows] = await devaPool.query(`
       SELECT * 
       FROM Personal_Turnado 
-      WHERE Iniciales NOT IN ('DEVA', 'DIRECCION');
+      WHERE	Pk_IDPersonalTurnado IN(1,2,3,4,5);
     `);
     res.json({ data: rows });
   } catch (error) {
