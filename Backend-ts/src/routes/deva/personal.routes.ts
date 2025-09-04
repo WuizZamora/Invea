e147920890110bed2c9d12a9f6056ff32ee55901
wuiz <wuiz@wuiz.inveadf.gob.mx>
2025-09-04 13:23:22 -0600

    MARIANO EN SELECT DEVACAP

--- Backend-ts/src/routes/deva/personal.routes.ts
+++ Backend-ts/src/routes/deva/personal.routes.ts
@@ -19,7 +19,7 @@ router.get('/personal-turnado', async (req, res) => {
     const [rows] = await devaPool.query(`
       SELECT * 
       FROM Personal_Turnado 
-      WHERE	Pk_IDPersonalTurnado IN(1,2,3,4,5);
+      WHERE	Pk_IDPersonalTurnado IN(1,2,3,4,5,30);
     `);
     res.json({ data: rows });
   } catch (error) {
